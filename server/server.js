86a4f5954ac602cbf3fc67c0ef8168be6c85720e
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-28 01:20:43 -0400

    refactored the server js file and webpack.config.server.js

--- server/server.js
+++ server/server.js
@@ -1,42 +1,21 @@
-import path from 'path';
-import express from 'express';
-import {MongoClient} from 'mongodb';
-const dotenv = require('dotenv');
-dotenv.config();
-import template from './../template';
+import config from './config/config';
+import app from './express';
+import mongoose from 'mongoose';
 
-//comment out before building for production
-import devBundle from './devBundle';
-import morgan from 'morgan';
-
-const app = express();
-//comment out before building for production
-devBundle.compile(app);
-app.use(morgan('dev'));
-
-const CURRENT_WORKING_DIR = process.cwd();
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
-
-app.get('/', (req, res) => {
-  res.status(200).send(template());
+// Connection URL
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
-let port = process.env.PORT || 9018;
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.info('Server started on port %s.', port);
+  console.info('Server started on port %s.', config.port);
 });
-
-// Database Connection URL
-const url = process.env.MONGODB_URI;
-// Use connect method to connect to the server
-MongoClient.connect(
-  url,
-  {useNewUrlParser: true, useUnifiedTopology: true},
-  (err, db) => {
-    console.log('Connected successfully to mongodb server');
-    db.close();
-  }
-);
